fa693496bd346ad298aafe82ca7eef768dda11f4
sebastian thielke <seb@redboatopera.com>
2019-01-24 15:44:21 +0100

    deleted and renamed a lot of stuff

--- resources/assets/js/dummy/permissions.js
+++
@@ -1,9 +0,0 @@
-
-
-export const permissions = [
-	{ id: 42, name: 'administrator.services' },
-	{ id: 55, name: 'administrator.vessels' },
-	{ id: 94, name: 'administrator.ports' },
-	{ id: 201, name: 'administrator.terminals' },
-	{ id: 4499, name: 'administrator.schedules' }
-]
