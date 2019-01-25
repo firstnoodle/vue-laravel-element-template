e7dc91225d90322b24dc3627397977e12a13e5c0
sebastian thielke <seb@redboatopera.com>
2019-01-25 14:13:33 +0100

    refactored utils -> service version name function to use regexes from regex.js instead

--- resources/assets/js/utils.js
+++ resources/assets/js/utils.js
@@ -1,3 +1,4 @@
+import {SERVICE_VERSION_REGEX, SERVICE_ALLIANCE_REGEX, SERVICE_CARRIER_REGEX} from './regex.js'
 
 /****************************************************** 
   Mixed goodies
@@ -93,27 +94,21 @@ export const setUrlParams = (params, path) => {
    Legacy stuff needs to be refactored
  ******************************************************/
 
-window.utils = {
-    regex: {
-        version: /^#(\d)/,
-        alliance: /\|{2} (\S{2,36}) - (\S{2,36}) \|{2}/,
-        carriers: /(\S{2,36}) - (\S{2,36})/g 
-    },
-    parseServiceName: serviceName => {
-        let version = utils.regex.version.test(serviceName) ? parseInt(utils.regex.version.exec(serviceName)[1]) : null;
-        // get alliance (alliance name and service name)
-        let allianceResult = utils.regex.alliance.test(serviceName) ? utils.regex.alliance.exec(serviceName) : null;
-        let alliance = allianceResult ? {name: allianceResult[1], service: allianceResult[2]} : null;
-        // for carriers we do a string.match
-        // that's the only (?) way to do a regex search for multiple occurences of the same pattern
-        let carriers = [];
-        let carrierResult = serviceName.match(utils.regex.carriers);
-        // if the name contains an alliance, then we should start at 1
-        let i = allianceResult ? 1 : 0;
-        for(;i < carrierResult.length; i++) {
-            let split = carrierResult[i].split(' - ');
-            carriers.push({name: split[0], service: split[1]});
-        }    
-        return { version, alliance, carriers };
-    }
-};
+export const parseServiceName = serviceName => {
+	let version = SERVICE_VERSION_REGEX.test(serviceName) ? parseInt(SERVICE_VERSION_REGEX.exec(serviceName)[1]) : null;
+	// get alliance (alliance name and service name)
+	let allianceResult = SERVICE_ALLIANCE_REGEX.test(serviceName) ? SERVICE_ALLIANCE_REGEX.exec(serviceName) : null;
+	let alliance = allianceResult ? {name: allianceResult[1], service: allianceResult[2]} : null;
+	// for carriers we do a string.match
+	// that's the only (?) way to do a regex search for multiple occurences of the same pattern
+	let carriers = [];
+	let carrierResult = serviceName.match(SERVICE_CARRIER_REGEX);
+	// if the name contains an alliance, then we should start at 1
+	let i = allianceResult ? 1 : 0;
+	for(;i < carrierResult.length; i++) {
+		let split = carrierResult[i].split(' - ');
+		carriers.push({name: split[0], service: split[1]});
+	}    
+	return { version, alliance, carriers };
+}
+
