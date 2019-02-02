36ff346b1bc85dc6e55b33cf90e679a4d8978ec5
sebastian thielke <seb@redboatopera.com>
2019-02-02 22:47:19 +0100

    SET_FORECASTS mutation

---
+++ resources/assets/js/store/SortableList/mutation-types.js
@@ -0,0 +1,2 @@
+
+export const SET_FORECASTS = 'SET_FORECASTS'
--- resources/assets/js/store/SortableList/mutations.js
+++ resources/assets/js/store/SortableList/mutations.js
@@ -1,4 +1,7 @@
+import * as types from './mutation-types.js'
 
 export const mutations = {
-	
+	[types.SET_FORECASTS] (state, forecasts) {
+		state.forecasts = forecasts
+	}	
 }
