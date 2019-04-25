e2346ffe1a18a46e561420bbb794f7a35cd1a57c
sebastian thielke <seb@firstnoodle.io>
2019-04-25 20:45:33 +0200

    created a fields property on each column to be able to hold several datafields in a column

--- resources/assets/js/store/DataTable/state.js
+++ resources/assets/js/store/DataTable/state.js
@@ -4,82 +4,123 @@ import {filterOperators} from 'root/components/DataTables/constants.js'
 export const state = {
 	columns: [
 		{
-			label: 'Service',
-			path: 'service.name',
+			name: 'Service',
 			layout: { xs: 6, sm: 6, md: 6, lg: 6, xl: 6 },
 			classes: ['is-highlighted'],
-			filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
-			filterType: 'Select',
-			filterValueOptions: null,
-			sortingFunctions: {
-				ascending: sort.stringAscending,
-				descending: sort.stringDescending
-			}
+			fields: [
+				{
+					label: 'Service',
+					path: 'service.name',
+					filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
+					filterType: 'Select',
+					filterValueOptions: null,
+					sortingFunctions: {
+						ascending: sort.stringAscending,
+						descending: sort.stringDescending
+					}
+				},
+				{
+					label: 'Vessel',
+					path: 'vessel.name',
+					filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
+					filterType: 'Select',
+					filterValueOptions: null,
+					sortingFunctions: {
+						ascending: sort.stringAscending,
+						descending: sort.stringDescending
+					}
+				}
+			]
 		},
 		{
-			label: 'Vessel',
-			path: 'vessel.name',
+			name: 'Vessel',
 			layout: { xs: 3, sm: 3, md: 3, lg: 3, xl: 3 },
 			classes: [],
-			filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
-			filterType: 'Select',
-			filterValueOptions: null,
-			sortingFunctions: {
-				ascending: sort.stringAscending,
-				descending: sort.stringDescending
-			}
+			fields: [
+				{
+					label: 'Vessel',
+					path: 'vessel.name',
+					filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
+					filterType: 'Select',
+					filterValueOptions: null,
+					sortingFunctions: {
+						ascending: sort.stringAscending,
+						descending: sort.stringDescending
+					}
+				}
+			]
 		},
 		{
-			label: 'Monitored port',
-			path: 'user_service_proforma.port.name',
+			name: 'Monitored port',
 			layout: { xs: 3, sm: 3, md: 3, lg: 3, xl: 3 },
 			classes: [],
-			filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
-			filterType: 'Select',
-			filterValueOptions: null,
-			sortingFunctions: {
-				ascending: sort.stringAscending,
-				descending: sort.stringDescending
-			}
+			fields: [
+				{
+					label: 'Monitored port',
+					path: 'user_service_proforma.port.name',
+					filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
+					filterType: 'Select',
+					filterValueOptions: null,
+					sortingFunctions: {
+						ascending: sort.stringAscending,
+						descending: sort.stringDescending
+					}
+				}
+			]
 		},
 		{
-			label: 'Next port',
-			path: 'next_port.name',
+			name: 'Next port',
 			layout: { xs: 3, sm: 3, md: 3, lg: 3, xl: 3 },
 			classes: [],
-			filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
-			filterType: 'Select',
-			filterValueOptions: null,
-			sortingFunctions: {
-				ascending: sort.stringAscending,
-				descending: sort.stringDescending
-			}
+			fields: [	
+				{
+					label: 'Next port',
+					path: 'next_port.name',
+					filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
+					filterType: 'Select',
+					filterValueOptions: null,
+					sortingFunctions: {
+						ascending: sort.stringAscending,
+						descending: sort.stringDescending
+					}
+				}
+			]
 		},
 		{
-			label: 'Subscriber',
-			path: 'user_service_proforma.user.name',
+			name: 'Subscriber',
 			layout: { xs: 3, sm: 3, md: 3, lg: 3, xl: 3 },
 			classes: [],
-			filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
-			filterType: 'Select',
-			filterValueOptions: null,
-			sortingFunctions: {
-				ascending: sort.stringAscending,
-				descending: sort.stringDescending
-			}
+			fields: [	
+				{
+					label: 'Subscriber',
+					path: 'user_service_proforma.user.name',
+					filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
+					filterType: 'Select',
+					filterValueOptions: null,
+					sortingFunctions: {
+						ascending: sort.stringAscending,
+						descending: sort.stringDescending
+					}
+				}
+			]
 		},
 		{
-			label: 'Responsible',
-			path: 'responsible_user.name',
+			name: 'Responsible',
 			layout: { xs: 3, sm: 3, md: 3, lg: 3, xl: 3 },
 			classes: [],
-			filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
-			filterType: 'Select',
-			filterValueOptions: null,
-			sortingFunctions: {
-				ascending: sort.stringAscending,
-				descending: sort.stringDescending
-			}
+			fields: [	
+				{
+					label: 'Responsible',
+					path: 'responsible_user.name',
+					filterOperators: [filterOperators.EQUAL, filterOperators.NOT_EQUAL],
+					filterType: 'Select',
+					filterValueOptions: null,
+					sortingFunctions: {
+						ascending: sort.stringAscending,
+						descending: sort.stringDescending
+					}
+				}
+			]
 		},
 	],
 	filters: [],
